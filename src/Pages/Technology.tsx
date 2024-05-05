22e2bda330c423357cb033f4b4dd30cae9dbdce4
Okocha Emeka Emmanuel <emekaokocha07@gmail.com>
2024-05-05 12:51:02 +0100

    update on view

--- src/Pages/Technology.tsx
+++ src/Pages/Technology.tsx
@@ -24,6 +24,7 @@ function Technology() {
     const handleResize =()=>{
       setWindowWidth(window.innerWidth)
     }
+    handleResize()
     window.addEventListener('resize',handleResize)
     return ()=>{
       window.removeEventListener('resize',handleResize)
